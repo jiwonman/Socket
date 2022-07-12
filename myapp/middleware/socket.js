06ab1d68d423c44ee563db411c225b33324a3f8a
jiwonman <kusa1230@naver.com>
2022-07-12 18:07:50 +0900

    test2

--- myapp/middleware/socket.js
+++ myapp/middleware/socket.js
@@ -7,12 +7,22 @@ module.exports = (server) => {
         const req = socket.request;
 
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        
         console.log('새로운 클라이언트 접속 !', ip, socket.id, req.ip);
 
         // socket.on('disconnect', () => {
         //     console.log('클라이언트 접속 해제', ip, socket.id);
         //     clearInterval(socket.interval);
         // });
+        socket.on('login', (data) => {
+            console.log('Client logged-in:\n name: ' + data.name + '\n userid: ' + data.userid);
+            
+            socket.name = data.name;
+            socket.userid = data.userid;
+
+            io.emit('login', data.name);
+        })
+
 
         socket.on('error', (error) => {
             console.log(error);
@@ -21,6 +31,21 @@ module.exports = (server) => {
         socket.on('reply', (data) => {
             console.log(data);
         });
+
+        socket.on('chat', (data) => {
+            console.log('Message from %s: %s', socket.name, data.msg);
+
+            let msg = {
+                from : {
+                name : socket.name,
+                userid : socket.userid
+            },
+            msg : data.msg
+        };
+        socket.broadcast.emit('chat', msg);
+        socket.emit('chat', msg);
+    });
+        
 
         socket.on('chat message', (msg) => {
             console.log('msg : ' + msg);
